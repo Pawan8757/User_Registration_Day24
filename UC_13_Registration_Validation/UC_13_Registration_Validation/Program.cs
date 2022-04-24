e00528a4b44e50787fdd2fcf2cd1bdf4c6f8a487
Pawan8757 <pawanyadavrfj2@gmail.com>
2022-04-24 22:29:45 +0530

    adding user registration refactor validation

---
+++ UC_13_Registration_Validation/UC_13_Registration_Validation/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace validate;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        string firstName = Console.ReadLine();
+        Console.WriteLine("Enter the last name = ");
+        string lastName = Console.ReadLine();
+        Console.WriteLine("Enter the email = ");
+        string email = Console.ReadLine();
+        Console.WriteLine("Enter the mobile pattern = ");
+        string mobilePattern = Console.ReadLine();
+        Console.WriteLine("Enter the password = ");
+        string preDefinedPassword = Console.ReadLine();
+
+
+        RegValidation check = new RegValidation();
+
+        Console.WriteLine(check.validateFirstName(firstName));
+        Console.WriteLine(check.validateLastName(lastName));
+        Console.WriteLine(check.validateEmail(email));
+        Console.WriteLine(check.validateMobileFormat(mobilePattern));
+        Console.WriteLine(check.validatePreDefinedPassword(preDefinedPassword));
+    }
+}
